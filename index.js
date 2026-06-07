1227e2fa2aef93678184be323225ab8995cc11ec
kitzarican <kitzarican@gmail.com>
2026-06-07 00:09:43 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click', function () {
+    alert('perdón perdón perdón mi vida te amo mucho');
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+    noBtn.style.setProperty(
+        'transform',
+        `translate(-${randomX}%, -${randomY}%)`
+    );
+});
